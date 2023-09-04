d6b017518016a74d08cbdb1e3d33f295aad24c29
Azeem Shaikh <82161544+azshhh@users.noreply.github.com>
2023-09-05 01:27:38 +0530

    My first API

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
-console.log("Hello World!");
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Listening on port ${port}...`));
