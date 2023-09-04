ec4af542fa9af54d45b809990c99184ad67ecdce
Azeem Shaikh <82161544+azshhh@users.noreply.github.com>
2023-09-05 01:52:28 +0530

    HTTP POST executed

--- routes/genres.js
+++ routes/genres.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
 
+router.use(express.json());
+
 const genres = [
     { id: 1, name: 'Action' },
     { id: 2, name: 'Comedy' },
@@ -14,7 +16,21 @@ router.get('/', (req, res) => {
 router.get('/:id', (req, res) => {
     const genre = genres.find(g => g.id === parseInt(req.params.id));
     if (!genre) return res.status(404).send('Course with given id was not found!');
+    res.send(genre);
+});
+
+router.post('/', (req, res) => {
+    // Add request genre to genres array
+    const genre = {
+        id: genres.length + 1,
+        name: req.body.name // we need a middleware
+    }
+
+    // Push genre to genres
+    genres.push(genre);
     res.send(genres);
 });
+
+
 
 module.exports = router;
