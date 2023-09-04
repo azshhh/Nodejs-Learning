8a304818d48650eb75441f67130d1e1f42bc5fd1
Azeem Shaikh <82161544+azshhh@users.noreply.github.com>
2023-09-05 01:39:29 +0530

    Handling GET request

--- routes/genres.js
+++ routes/genres.js
@@ -11,4 +11,10 @@ router.get('/', (req, res) => {
     res.send(genres);
 });
 
+router.get('/:id', (req, res) => {
+    const genre = genres.find(g => g.id === parseInt(req.params.id));
+    if (!genre) return res.status(404).send('Course with given id was not found!');
+    res.send(genres);
+});
+
 module.exports = router;
