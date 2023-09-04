5add332a44c8db11a05673f58c94480fae7d1bf1
Azeem Shaikh <82161544+azshhh@users.noreply.github.com>
2023-09-05 01:33:26 +0530

    HTTP GET reqest for custom genres

--- main.js
+++ main.js
@@ -1,9 +1,8 @@
 const express = require('express');
 const app = express();
+const genres = require('./routes/genres');
 
-app.get('/', (req, res) => {
-    res.send('Hello World!')
-});
+app.use('/api/genres', genres);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
