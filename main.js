a7c68d49e7ae069699f1af05581c8418cbd92844
Azeem Shaikh <82161544+azshhh@users.noreply.github.com>
2023-09-05 11:45:35 +0530

    Adding static files to node application

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ const app = express();
 const genres = require('./routes/genres');
 
 app.use('/api/genres', genres);
+app.use(express.static('public'))
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
---
+++ public/readme.txt
@@ -0,0 +1,1 @@
+This is how we add static files to node application.
